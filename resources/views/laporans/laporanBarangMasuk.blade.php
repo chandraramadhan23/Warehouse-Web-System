1ccf2c3fbd2fdc920e1bfd8f21e34d4122ca98b9
Muhammad Chandra Ramadhan <82149114+chandraramadhan23@users.noreply.github.com>
2024-07-07 13:23:56 +0700

    add report in product

--- resources/views/laporans/laporanBarangMasuk.blade.php
+++ resources/views/laporans/laporanBarangMasuk.blade.php
@@ -15,14 +15,6 @@
                 </div>
 
                 <div class="row">
-                    <div class="col-12 mb-4 d-flex justify-content-end">
-                        <button class="btn btn-success">
-                            <i class="ri-printer-line me-2"></i>Print Laporan
-                        </button>
-                    </div>
-                </div>
-
-                <div class="row">
                     <div class="col-lg-12">
                         <div class="card px-4 py-4">
                             <table id="tableLaporanBarangMasuk" class="table table-hover nowrap align-middle" style="width:100%">
