5f49457ecb73820977f420524dbcaceda4ac6e27
Muhammad Chandra Ramadhan <82149114+chandraramadhan23@users.noreply.github.com>
2024-07-07 10:19:09 +0700

    add stok barang

--- resources/views/stokBarang.blade.php
+++ resources/views/stokBarang.blade.php
@@ -14,6 +14,12 @@
                     </div>
                 </div>
 
+                <div class="row mb-3">
+                    <div class="col-4">
+                        <button id="addSupplierModal" class="btn btn-primary">Tambah Barang</button>
+                    </div>
+                </div>
+
                 <div class="row">
                     <div class="card col-lg-8 px-4 py-4">
                         <table id="tableStokBarang" class="table nowrap align-middle" style="width:100%">
@@ -23,6 +29,7 @@
                                     <th data-ordering="false">Kategori</th>
                                     <th data-ordering="false">Nama Barang</th>
                                     <th data-ordering="false">Jumlah</th>
+                                    <th data-ordering="false">Action</th>
                                 </tr>
                             </thead>
                         </table>
@@ -37,6 +44,9 @@
 
 {{-- Show Datatable --}}
 @section('table')
+@section('modal')
+@include('modals.modal')
+
     <script>
 
         let table = $('#tableStokBarang').DataTable({
@@ -57,10 +67,52 @@
                 {data: 'categoryName'},
                 {data: 'productName'},
                 {data: 'amount'},
+                {
+                    width: '20%',
+                    render:function(data, type, row) {
+                            return `
+                                <button class='btn btn-info edit' data-id="${row.id}" data-suplliername="${row.supplierName}" data-alamat="${row.alamat}" data-nohp="${row.noHp}">Edit</button>
+                                <button class='btn btn-danger delete' data-id='${row.id}'>Delete</button>
+                            `
+                        }
+                }
             ],
         })
 
+
+        // Add
+        $(document).on('click', '#addSupplierModal', function() {
+            $('#containerModal').empty().append(`
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambah Barang</h5>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label class="col-form-label">Kategori :</label>
+                            <input type="text" class="form-control" id="supplierName">
+                        </div>
+                        <div class="mb-3">
+                            <label class="col-form-label">Nama Barang :</label>
+                            <input type="text" class="form-control" id="supplierName">
+                        </div>
+                        <div class="mb-3">
+                            <label class="col-form-label">Jumlah :</label>
+                            <input type="text" class="form-control" id="supplierName">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="submitAddSupplier">Submit</button>
+                </div>
+            `)
+
+            $('#modal').modal('show')
+        })
+
     </script>
+@endsection
 @endsection
 
 
